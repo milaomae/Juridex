9ac4b5437a446737b05da1a171d0ab0739158ae0
milaomae <31636124+milaomae@users.noreply.github.com>
2017-11-28 21:27:06 -0200

    update tela de resultado

--- app/src/main/java/com/example/lucas/juridex_v13/Game/FimJogoFragment.java
+++ app/src/main/java/com/example/lucas/juridex_v13/Game/FimJogoFragment.java
@@ -35,11 +35,98 @@ public class FimJogoFragment extends Fragment{
         btnProximoTeste = view.findViewById(R.id.btnProximoTeste);
         btnVoltarTelaNiveis = view.findViewById(R.id.btnVoltarJustificativa);
         btnReiniciarTeste = view.findViewById(R.id.btnReiniciarTeste);
+        
+        //set textos
+        setTxts();
+        
+        //cliques dos botões
+        setClicks();
+        
+        //setar valores das mensagens
+        
+        
 
         return view;
     }
+    
+    public vois setTxts(){        
+        if(Common.getAcertos() == Common.getQuestoesTotais())
+            txtMensagem.setText("UOU"); //acertou tudo
+        else    
+            if(Common.getAcertos() >= (Common.getQuestoesTotais()/2))
+                txtMensagem.setText("Esta bom, mas pode melhorar!"); //acertou metade ou mais que a metade
+            else
+                txtMensagem.setText("Poxa, está quase lá!"); //acertou menos que a metade
+        
+        result.setText(Common.getAcertos() + "  /  " + Common.getQuestoesTotais());
+        
+    }
 
     public void setClicks(){
-
+        
+        btnProximoTeste.setOnClickListener(new View.OnClickListener(){
+          @Override
+            public void onClick(View view) {
+                //settar variável comum referente ao nível
+                if(Common.getNivel().equals("cdc_easy")){
+                    Common.setNivel("cdc_medium");
+                    //redireciona para a página de questões, porém no nível médio
+                    ((GameActivity)getActivity()).setViewPager(0);
+                }
+                else
+                    if(Common.getNivel().equals("cdc_medium")){
+                        Common.setNivel("cdc_hard");
+                        //redireciona para a página de questões, porém no nível médio
+                        ((GameActivity)getActivity()).setViewPager(0);
+                    }
+                    else
+                        if(Common.getNivel().equals("cdc_hard")){
+                            //dialog, esperar próximos níveis
+                            MaterialDialog dialog = new MaterialDialog.Builder(getActivity())
+                                .title("Você já fez todos os testes!")
+                                .content("Aguarde novos e envie sugestões (:")
+                                .positiveText("Fechar")
+                                .show();
+                            
+                            dialog.getActionButton(DialogAction.POSITIVE).setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                dialog.dismiss();
+                                //encerra o fragment e volta para a tela de níveis(Game Activity)
+                                getActivity().getFragmentManager().popBackStack();
+                                }
+                              });                            
+                            }                
+                
+                Toast.makeText(getActivity(), "Próximo Teste (nível médio) ", Toast.LENGTH_SHORT ).show();
+            }
+        
+        });
+        
+        btnVoltarTelaNiveis.setOnClickListener(new View.OnClickListener(){
+          @Override
+            public void onClick(View view) {
+               
+                //encerra o fragment e volta para a tela de níveis(Game Activity)
+                getActivity().getFragmentManager().popBackStack();
+                
+                Toast.makeText(getActivity(), "encerrou fragment", Toast.LENGTH_SHORT ).show();
+            }
+        
+        });
+        
+        btnReiniciarTeste.setOnClickListener(new View.OnClickListener(){
+          @Override
+            public void onClick(View view) {
+               //redireciona para a página de questões no mesmo nível atual
+               ((GameActivity)getActivity()).setViewPager(0);                
+                
+                Toast.makeText(getActivity(), "encerrou fragment", Toast.LENGTH_SHORT ).show();
+            }
+        
+        });
+        
     }
+    
+   
 }
